6003d255f6fd5b4088f80e0b872e07a59bc0de6d
Jyoti <jyoticherharval12@gmail.com>
2021-06-04 22:53:40 +0530

    added backend to load products from backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import data from './data.js';
+
+const app=express();
+
+app.get('/api/products', (req,res) =>{
+    res.send(data.product);
+});
+app.get('/', (req,res) => {
+    res.send("sever is ready");
+});
+
+const port = process.env.PORT || 5000; 
+app.listen(port, () =>{
+    console.log(`server running on http://localhost:${port}`);
+} )
